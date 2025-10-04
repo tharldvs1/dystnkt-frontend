739f6c404191787e40d0fad31d10beebd4c29342
Edward Meaderds <tharldvs1@yahoo.com>
2025-10-03 23:06:47 -0500

    Remove Tailwind PostCSS diagnostic from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-console.log("Attempting to require @tailwindcss/postcss:", require('@tailwindcss/postcss'));
 
   /* config options here */
 };
