49d6a9eed4c2d6e5ba5b6162686042d5c3dbc362
Edward Meaderds <tharldvs1@yahoo.com>
2025-10-03 22:55:45 -0500

    Add Tailwind PostCSS diagnostic to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+console.log("Attempting to require @tailwindcss/postcss:", require('@tailwindcss/postcss'));
+
   /* config options here */
 };
 
